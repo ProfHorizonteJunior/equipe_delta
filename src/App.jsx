eed93e6c7eb0311d316e27e8b2a6ecaa7f3bd913
TulioSilveiraLodetti <tulio.lodetti1@gmail.com>
2026-05-05 21:45:59 -0300

    mudando layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
-git 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CryptoMonitor from "./pages/CryptoMonitor";
+import { Secrets } from "./pages/Secrets";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CryptoMonitor />} />
+          <Route path="/secrets" element={<Secrets />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
